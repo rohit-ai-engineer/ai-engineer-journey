57f1e3eddc00898da893e368284e1e844e962bca
rohit-ai-engineer <rohitmasteringai@gmail.com>
2026-02-14 21:21:48 +0530

    feat(week1): build calculator - first python program

---
+++ phase-1-foundations/week-01-python-basics/calculator.py
@@ -0,0 +1,20 @@
+# Simple Calculator - Week 1 Project
+
+print ("Welcome to my calculator!")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+             
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("--- Results ---")
+print("Addition:      ", addition)
+print("Subtraction:   ", subtraction)
+print("Multiplication:", multiplication)
+print("Division:      ", division)
+
+
